b51de49e686f25cebd67b601267e2ff88d78eee0
Cong Phuong <bonanhphuong@gmail.com>
2024-11-02 00:57:37 +0700

    Database connection

---
+++ src/main/java/com/example/JWT_version2/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.JWT_version2.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
