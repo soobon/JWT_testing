7d47aa1be74f764d344efbe8aa88d3d1a13bab1c
Cong Phuong <bonanhphuong@gmail.com>
2024-11-02 00:24:02 +0700

    Initial commit

---
+++ src/main/java/com/example/JWT_version2/JwtVersion2Application.java
@@ -0,0 +1,13 @@
+package com.example.JWT_version2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtVersion2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtVersion2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JWT_version2/JwtVersion2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JWT_version2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtVersion2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
